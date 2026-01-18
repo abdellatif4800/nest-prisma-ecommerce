6a37526c88a43a5344397d925ed89b7d796b4b69
abdellatif4800 <mabdellatif82000@gmail.com>
2026-01-18 18:42:10 +0200

    last update for configration files

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   schema: './libs/prisma-setup/prisma/schema/',
   migrations: {
     path: './libs/prisma-setup/prisma/migrations',
+    // seed: 'pnpm dlx tsx ./libs/prisma-setup/src/seed/seed.ts',
+    seed: 'pnpm dlx tsx ./libs/prisma-setup/src/seed/seedWithRawSql/seedWithRawSql.ts',
   },
   datasource: {
     url: process.env['DATABASE_URL'],
